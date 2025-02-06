24235a338b80cd8d82921e00485e874f288716a6
b4n <74839706+ogb4n@users.noreply.github.com>
2025-02-06 20:53:00 +0100

    #2 bugfix item edit (#14)
    * layout addition
    
    * fix: addItem route
    
    * fix: edit working without places&rooms
    
    * code cleaning
    
    * gitignore additions

---
+++ src/app/(pages)/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import Navbar from "@/app/components/shared/Navbar";
+import React from "react";
+import { AuthProvider } from "../contexts/authContext";
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+export const metadata: Metadata = {
+  title: "ShelfSpot",
+  description: "Keep an eye on your stocks",
+};
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        style={{ margin: 0, padding: 0, overflow: "hidden" }}
+      >
+        <div style={{ display: "flex", height: "100vh" }}>
+          <header style={{ flexShrink: 0 }}>
+            <Navbar />
+          </header>
+          <div style={{ flexGrow: 1, overflow: "auto" }}>{children}</div>
+        </div>
+      </body>
+    </html>
+  );
+}
--- src/app/api/items/edit/route.ts
+++ src/app/api/items/edit/route.ts
@@ -4,28 +4,21 @@ import { prisma } from "@/app/utils/prisma";
 export async function POST(req: Request) {
   const body = await req.json();
 
-  // Vérifiez si `placeId` est fourni
-  if (!body.placeId) {
+  if (!body || typeof body !== "object") {
     return NextResponse.json(
-      { error: "placeId is required." },
+      { error: "Invalid request body" },
       { status: 400 }
     );
   }
 
   try {
     const item = await prisma.item.update({
-      where: {
-        id: body.id, // Identifiant de l'objet à mettre à jour
-      },
+      where: { id: body.id },
       data: {
-        name: body.name,
-        stock: body.stock,
-        price: body.price,
-        room: {
-          connect: { id: body.roomId }, // Connecte la room si `roomId` est fourni
-        },
-        placeId: body.placeId, // Met à jour l'ID de la place
-        status: body.status,
+        name: body.name ?? "",
+        stock: Number(body.stock) || 0,
+        placeId: body.placeId,
+        status: body.status ?? "",
       },
     });
 
