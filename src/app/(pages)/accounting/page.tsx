5c38a34d817582b308581ad424031a2bac6c8241
ogb4n <ogb4n@protonmail.com>
2025-02-23 22:35:14 +0100

    accounting page

---
+++ src/app/(pages)/accounting/page.tsx
@@ -0,0 +1,11 @@
+"use server";
+
+const Accounting = () => {
+  return (
+    <main className="flex min-h-screen items-center justify-center p-24">
+      <h1 className="text-4xl font-bold">Accounting</h1>
+    </main>
+  );
+};
+
+export default Accounting;
