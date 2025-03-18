c1712a257f5bde92070f449935c69fdd178c106f
ogb4n <ogb4n@protonmail.com>
2025-03-18 19:38:00 +0100

    fix: components removal

--- src/app/(pages)/accounting/page.tsx
+++ src/app/(pages)/accounting/page.tsx
@@ -1,10 +1,8 @@
 "use server";
-import { WalletManager } from "@/app/components/WalletManager";
 
 const Accounting = () => {
   return (
     <main className="flex min-h-screen items-center justify-center p-24">
-      {/* <WalletManager /> */}
       <h2 className="text-red-500">
         This functionality have not been implemented yet.
       </h2>
